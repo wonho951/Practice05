6bc18326124322f686345e48a0517a7695987b8d
wonho <wonho951@gmail.com>
2021-06-04 16:31:53 +0900

    2021.06.04 객체지향 과제

--- src/com/javaex/ex03/Song.java
+++ src/com/javaex/ex03/Song.java
@@ -2,7 +2,39 @@ package com.javaex.ex03;
 
 public class Song {
 	
+	//필드
+	private String title;
+	private String artist;
+	private String album;
+	private String composer;
+	private String year;
+	private int track;
 	
+	//생성자
+
+	
+	//메소드gs
+	
+	//메소드-일반
+	
+	public Song(){
+		System.out.println("Song(파라미터0개)");
+	}
+
+	public Song(String title, String artist, String album, String composer, String year) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.composer = composer;
+		this.year = year;
+		System.out.println("Song(파라미터5개)");
+	}
+
+	public Song(String title, String artist, String album, String composer, String year, int track) {
+		this(title, artist, album, composer, year);
+		this.track = track;
+		System.out.println("Song(파라미터6개)");
+	}
 	
 }
 
--- src/com/javaex/ex03/SongApp.java
+++ src/com/javaex/ex03/SongApp.java
@@ -4,9 +4,13 @@ public class SongApp {
 
 	public static void main(String[] args){
 		
+		Song s1 = new Song("좋은날", "아이유", "Real", "이민수" , "2010",3);
 		
 	}
 
 }
 
 
+//출력 결과를 예상하여 작성하세요
+
+// 파라미터0,5개는 출력 안될것같다. --> SongApp에서 new연산자 사용해서 파라미터 안에 6개 집어넣었기 때문에..? 
