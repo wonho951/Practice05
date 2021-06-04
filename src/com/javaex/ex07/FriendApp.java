9e10f8d93de922dc60e319fc982a430b063a098a
wonho <wonho951@gmail.com>
2021-06-04 17:46:20 +0900

    2021.06.04 객체지향 과제

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -14,23 +14,32 @@ public class FriendApp {
         //친구정보 3명 입력 로직 --> 반복문 사용
         for( int i = 0; i < friendArray.length; i++) {    
         	// 친구정보(이름) 입력받기
-        	System.out.println("이름 : ");
-        	String name = sc.nextLine();         	
+        	System.out.print("이름 : ");
+        	String name = sc.nextLine();  
+        	
         	// 친구정보(핸드폰) 입력받기
-        	System.out.println("핸드폰 : ");
-        	String hp = sc.nextInt();
+        	System.out.print("핸드폰 : ");
+        	String hp = sc.nextLine();
+        	
         	// 친구정보(학교) 입력받기
-        	System.out.println("학교 : ");
+        	System.out.print("학교 : ");
+        	String school = sc.nextLine();
+        	System.out.println("----------------------------------");
             // Friend 인스턴스 생성하여 데이터 넣기
-
+        	Friend info = new Friend();
+        	
+        	info.setName(name);
+        	info.setHp(hp);
+        	info.setSchool(school);        	        	
             // 인스턴스 주소를 배열에 대입
-
+        	friendArray[3] = info;
         }
         
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
             //친구정보 출력 메소드 호출
+        	
         }
 
         sc.close();
