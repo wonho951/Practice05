6bec8df2a2ff6bad97dd785aeb5dedb4a9e43226
wonho <wonho951@gmail.com>
2021-06-04 17:34:00 +0900

    2021.06.04 객체지향 과제

--- src/com/javaex/ex07/Friend.java
+++ src/com/javaex/ex07/Friend.java
@@ -1,17 +1,39 @@
 package com.javaex.ex07;
 
 public class Friend {
-
+	//필드 
     private String name;
     private String hp;
     private String school;
 
     
     // getter/setter 작성
-    
-    
-    
-    public void showInfo(){
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+	//메소드-일반
+	public void showInfo(){
         System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
     }
 
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -12,17 +12,21 @@ public class FriendApp {
         System.out.println("친구를 3명 등록해 주세요");
 
         //친구정보 3명 입력 로직 --> 반복문 사용
-            
+        for( int i = 0; i < friendArray.length; i++) {    
         	// 친구정보(이름) 입력받기
-
+        	System.out.println("이름 : ");
+        	String name = sc.nextLine();         	
         	// 친구정보(핸드폰) 입력받기
-        
+        	System.out.println("핸드폰 : ");
+        	String hp = sc.nextInt();
         	// 친구정보(학교) 입력받기
-
+        	System.out.println("학교 : ");
             // Friend 인스턴스 생성하여 데이터 넣기
 
             // 인스턴스 주소를 배열에 대입
 
+        }
+        
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
