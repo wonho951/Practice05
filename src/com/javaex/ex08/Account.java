e9b57a07059c8c075cb16a4dbf62c28f9552924d
wonho <wonho951@gmail.com>
2021-06-05 21:52:56 +0900

    2021.06.05 객체지향 과제

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -4,9 +4,15 @@ public class Account {
 
     private String accountNo;
     private int balance;
+
     
     //생성자 작성
+	public Account(String accountNo, int balance) {
+		super();
+		this.accountNo = accountNo;
+		this.balance = balance;
+	}
     
     //필요한 메소드 작성
-
+	public void 
 }
