0078566dcb1195633ed2258b51e22410a9de6411
wonho <wonho951@gmail.com>
2021-06-06 17:30:03 +0900

    2021.06.06 객체지향 과제

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -2,7 +2,28 @@ package com.javaex.ex10;
 
 public class Book {
     
-    
+    //필드
+	private int bookNo;
+	private String title;
+	private String author;
+	private int stateCode;
+
+	
+	//생성자
+	public Book(int bookNo, String title, String author, int stateCode) {
+		super();
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+		this.stateCode = stateCode;
+	}
+	
+	
+	//메소드 g/s
+	
+	
+	
+	//메소드 - 일반
     
     
     
