0449eb6f8b7fe2aa3879ea7a27c7a5c129f179b7
wonho <wonho951@gmail.com>
2021-06-06 17:30:39 +0900

    2021.06.06 객체지향 과제

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -20,7 +20,44 @@ public class Book {
 	
 	
 	//메소드 g/s
-	
+	public int getBookNo() {
+		return bookNo;
+	}
+
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+	public int getStateCode() {
+		return stateCode;
+	}
+
+
+	public void setStateCode(int stateCode) {
+		this.stateCode = stateCode;
+	}
 	
 	
 	//메소드 - 일반
